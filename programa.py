5dd89949aa1525a745b2600ef2a295f87d8d9120
MemoLTS <guil.toledo@duocuc.cl>
2026-03-13 12:16:31 -0300

    name

---
+++ programa.py
@@ -0,0 +1,16 @@
+from os import system
+import time
+import sys
+system('cls')
+
+i=0
+def escribir_letra_por_letra(texto, retardo=0.1):
+    for letra in texto:
+        sys.stdout.write(letra)
+        sys.stdout.flush()
+        time.sleep(retardo)
+    print()  # Salto de línea al final
+
+while i<69:
+    escribir_letra_por_letra("Nigger", 0.18)
+    i=+1   
